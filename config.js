574e629a9fcb0c36121bb3b1cc49c451d1b23948
luanym1234vip <camduyen20012@gmail.com>
2022-10-20 16:59:39 +0700

    Dien

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+  introTitle: "Hello bà Nhê ngu ngốc à!",
+  introDesc: ``,
+  btnIntro: "Chó Nhê",
+  title: "Chúc em có 1 ngày 20/10 thật vui vẻ 😙",
+  desc: "Luôn hanh phúc bên anh nhen ",
+  btnYes: "Yêu bà Diên cute 😘😘",
+  btnNo: "Không, cút :v",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3",
+  btnReply: "Nói mau <3",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: "Sến súa quá mà kệ :3",
+  btnAccept: "Ahihi đồ chó <3", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
